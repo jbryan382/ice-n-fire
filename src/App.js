a4db63319f20822da84923cf73d5fe1a0844c592
Jordan Bryan <jbryan382@gmail.com>
2019-03-11 10:12:33 -0400

    Working on API call, might need nested req

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Axios from 'axios'
+
+// API URL = https://anapioficeandfire.com/api/characters/583
 
 class App extends Component {
+  state = {
+    character: [],
+    house: [],
+    id: []
+  }
+  componentDidMount() {
+    Axios.get(`https://anapioficeandfire.com/api/houses/362`).then(resp => {
+      console.log({ resp })
+      this.setState({
+        character: resp.data.character,
+        house: resp.data.house,
+        id: resp.data.id
+      })
+    })
+  }
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <header>
+          <h1>The House of Stark</h1>
+          <h2>The Main Nuclear Family</h2>
+          <ul>
+            <li />
+          </ul>
+        </header>
+      </>
+    )
   }
 }
 
